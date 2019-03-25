2e3158d6dc386712f0ee4b25012bf059c0bc7faa
Edwin Pratt <ttarped228@gmail.com>
2019-03-26 00:04:11 +0200

    Added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2019 Edwin Pratt
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/markov.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'markov.min.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ]
+        }
+      }
+    ]
+  }
+};
