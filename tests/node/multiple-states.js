16ae968fe2505445e4b59601f4fbd67b2f8cc412
Edwin Pratt <ttarped228@gmail.com>
2019-03-27 05:17:20 +0200

    All tests passed successful on Node.js

---
+++ tests/node/multiple-states.js
@@ -0,0 +1,14 @@
+const Markov = require('../../dist/markov.node.js');
+
+const markov = new Markov();
+
+// @ Thanks goes to: Patrick Henry to the Virginia Convention
+const txt = 'We have petitioned; we have remonstrated; we have supplicated; we have prostrated ourselves before the tyrannical hands of the ministry and parliament. Our petitions have been slighted; our remonstrances have produced additional violence and insult; our supplications have been disregarded; and we have been spurned, with contempt, from the foot of the throne.';
+
+const words = txt.split('; ');
+
+markov.addState(words);
+
+markov.train();
+
+console.log(markov.generate(500));
